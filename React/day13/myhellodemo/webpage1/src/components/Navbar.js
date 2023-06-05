2077c4332d34e3bbdb76f0023688420f767754c0
dac <siddhantmalik304@gmail.com>
2023-06-06 01:53:20 +0530

    employeemanagement

--- React/day13/myhellodemo/webpage1/src/components/Navbar.js
+++ React/day13/myhellodemo/webpage1/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <div>
+    <div id="d1">
      <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
     <a className="navbar-brand" href="/" >{props.title}</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
